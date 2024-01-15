dbd51caa9481bfd92df7bd244f13dd6b87aa3a95
carmelc <carmelc@wix.com>
2024-01-15 22:21:33 +0200

    add jwt validations

--- src/app/api/shipping-rates/v1/getRates/route.ts
+++ src/app/api/shipping-rates/v1/getRates/route.ts
@@ -1,28 +1,34 @@
+import {parseJwt} from "@/app/utils/jwt-verify";
 
 export async function POST(request: Request) {
   console.info('Shipping rates::POST - called');
-  const input = await request.text();
-  console.log('Shipping rates input: ', input);
+  const jwtPayload = await request.text();
+
+  // verify the data was not tampered with, and get the input
+  const input = parseJwt(jwtPayload, true);
+  console.log('Shipping rates::input: ', input);
+
+  // return the shipping rates, in this case it is static
   const data = {
     shippingRates: [
-      // {
-      //   code: 'example-shipping-rate',
-      //   title: 'Example Shipping Rate',
-      //   logistics: {
-      //     deliveryTime: '2-5 days'
-      //   },
-      //   cost: {
-      //     price: '15',
-      //     currency: 'USD',
-      //     additionalCharges: [
-      //       {
-      //         price: '10',
-      //         type: 'HANDLING_FEE',
-      //         details: 'Handling fee of $10 applied for fragile items.'
-      //       }
-      //     ]
-      //   }
-      // },
+      {
+        code: 'example-shipping-rate',
+        title: 'Example Shipping Rate',
+        logistics: {
+          deliveryTime: '2-5 days'
+        },
+        cost: {
+          price: '15',
+          currency: 'USD',
+          additionalCharges: [
+            {
+              price: '10',
+              type: 'HANDLING_FEE',
+              details: 'Handling fee of $10 applied for fragile items.'
+            }
+          ]
+        }
+      },
       {
         code: 'example-shipping-rate-premium',
         title: 'Example Shipping Rate Premium',
---
+++ src/app/utils/jwt-verify.ts
@@ -0,0 +1,39 @@
+import jwt, {VerifyOptions, Jwt, JwtPayload} from 'jsonwebtoken';
+
+const verifyOptions: VerifyOptions = {
+  complete: true,
+  algorithms: ['RS256'],
+  // Add optional verification options, such as algorithms, audience, issuer, etc.
+};
+
+const parseJwtKeyIfEncoded = (secretOrPublicKey: string): string => {
+    if (secretOrPublicKey.includes('\n') || secretOrPublicKey.includes('\r')) {
+        // secretOrPublicKey is multi-line string, can be used as is
+        return secretOrPublicKey.trim();
+    } else {
+        // secretOrPublicKey is base64 encoded
+        return Buffer.from(secretOrPublicKey, 'base64').toString('utf-8');
+    }
+}
+
+export const parseJwt = (token: string, verifyCallerClaims = false): object | null => {
+  const secretKey = process.env.WIX_APP_JWT_KEY;
+  if (!token || !secretKey) {
+    throw new Error('parseJwt:Missing token or secret key');
+  }
+  const result = jwt.verify(token, parseJwtKeyIfEncoded(secretKey), verifyOptions) as Jwt;
+  const payload = result.payload as JwtPayload;
+  if (verifyCallerClaims && (
+    payload.aud !== process.env.WIX_APP_ID ||
+    payload.iss !== 'wix.com'
+  )) {
+    throw new Error('parseJwt:seems that the token is not from the right caller');
+  }
+  if (
+    payload.exp! < Math.floor(Date.now() / 1000) &&
+    payload.iat! > Math.floor(Date.now() / 1000)
+  ) {
+    throw new Error('parseJwt:seems that the token is expired');
+  }
+  return payload.data;
+}
