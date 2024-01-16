3e24ba862603441773b72675d6d471c2d1c0ddf9
carmelc <carmelc@wix.com>
2024-01-16 16:40:22 +0200

    add webhooks

---
+++ src/app/api/webhooks/v1/install/route.ts
@@ -0,0 +1,11 @@
+import { type NextRequest } from 'next/server';
+import {parseJwt} from "@/app/utils/jwt-verify";
+
+export async function POST(request: NextRequest) {
+  console.info('Webhook::install - called');
+  const jwtPayload = await request.text();
+
+  // verify the data was not tampered with, and get the input
+  const input = parseJwt<any>(jwtPayload, false)!;
+  console.info('Webhook::install - input is', input);
+}
