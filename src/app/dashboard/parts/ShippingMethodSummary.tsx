687fd053aeb1ed0889554645ea3de3ff43b2ae66
carmelc <carmelc@wix.com>
2024-01-24 16:12:19 +0200

    make it pixel perfect

--- src/app/dashboard/parts/ShippingMethodSummary.tsx
+++ src/app/dashboard/parts/ShippingMethodSummary.tsx
@@ -18,40 +18,42 @@ export function ShippingMethodSummary({ orders }: { orders?: OrderSummary[] }) {
       <Card.Header title='Your last orders' />
       <Card.Divider />
       <Card.Content>
-        {orders?.length ? (
-          orders.map((order, index) => (
-            <>
-              {index > 0 && <Divider skin='light' />}
-              <Box key={index} verticalAlign='middle' padding='SP2' align='space-between'>
-                <Box direction='vertical'>
-                  <Text size='small' weight='normal'>
-                    Order #{order.id}
-                  </Text>
-                  <Text size='tiny' weight='thin' skin='disabled'>
-                    {new Date(order.createdDate).toLocaleDateString(LOCALE, {
-                      year: 'numeric',
-                      month: 'short',
-                      day: 'numeric',
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
+        <Box direction='vertical' paddingBottom='SP3'>
+          {orders?.length ? (
+            orders.map((order, index) => (
+              <>
+                {index > 0 && <Divider skin='light' />}
+                <Box key={index} verticalAlign='middle' align='space-between' paddingTop='SP2' paddingBottom='SP1'>
+                  <Box direction='vertical'>
+                    <Text size='small' weight='normal'>
+                      Order #{order.id}
+                    </Text>
+                    <Text size='tiny' weight='thin' skin='disabled'>
+                      {new Date(order.createdDate).toLocaleDateString(LOCALE, {
+                        year: 'numeric',
+                        month: 'short',
+                        day: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </Text>
+                  </Box>
+                  <Text size='tiny' weight='thin'>
+                    {new Intl.NumberFormat(LOCALE, {
+                      style: 'currency',
+                      currency: order.currency,
+                      minimumFractionDigits: 0,
+                    }).format(order.totalPrice)}
                   </Text>
                 </Box>
-                <Text size='tiny' weight='thin'>
-                  {new Intl.NumberFormat(LOCALE, {
-                    style: 'currency',
-                    currency: order.currency,
-                    minimumFractionDigits: 0,
-                  }).format(order.totalPrice)}
-                </Text>
-              </Box>
-            </>
-          ))
-        ) : (
-          <Text size='small' weight='thin'>
-            No orders found
-          </Text>
-        )}
+              </>
+            ))
+          ) : (
+            <Text size='small' weight='thin'>
+              No orders found
+            </Text>
+          )}
+        </Box>
         <Box align='right' paddingTop='SP1'>
           <TextButton onClick={() => navigate(ORDERS_PAGE_ID)} size='small' suffixIcon={<ArrowRight />}>
             Go to all orders
